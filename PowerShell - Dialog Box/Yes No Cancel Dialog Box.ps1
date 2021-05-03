289d5f97eeda208bb6d3bca292cb070d056a21a0
mr-tgokul <76623691+mr-tgokul@users.noreply.github.com>
2021-05-03 13:01:29 +0530

    Yes-No-Cancel Dialog Box

---
+++ PowerShell - Dialog Box/Yes No Cancel Dialog Box.ps1
@@ -0,0 +1,27 @@
+﻿$msgBoxInput =  [System.Windows.MessageBox]::Show('Would you like to play a game?','Game  input','YesNoCancel','Error')
+
+  switch  ($msgBoxInput) {
+
+  'Yes' {
+
+  ## Do something  
+       Write-Host "You have clicked Yes" -ForegroundColor Green
+
+  }
+
+  'No' {
+
+  ## Do something
+
+  Write-Host "You have clicked No" -ForegroundColor Red
+
+  }
+
+  'Cancel' {
+
+  ## Do something
+
+  Write-Host "You have clicked Cancel" -ForegroundColor Yellow
+  }
+
+  }
