b405e22875d3677b305b82e50d840b6c149bde02
Gokul T <mr.tgokul@gmail.com>
2021-07-26 17:45:46 +0530

    Reload-VM
    Function to Reload the VM without Registering the VM using its VMX file

---
+++ PowerCli Scripts/Reload-VM.ps1
@@ -0,0 +1,24 @@
+﻿Function Reload-VM 
+{
+<#
+ .SYNOPSIS
+ Function to Reload the Virtual machine
+
+ .DESCRIPTION
+ Function to Reload the Virtual machine without Registering it using the VMX file
+
+.EXAMPLE
+ Reload-VM -VM <VMNAME>
+
+[CmdletBinding()]
+param(
+[parameter(position=0,Mandatory=$True)]
+$VM
+)
+$VMCheck = get-vm -name $VM -ErrorAction SilentlyContinue
+If ($VMCheck){Get-VM -Name $VM | Get-View | %{$_.reload()}}
+Else {Write-Host "Plese enter the Correct VM Name" -ForegroundColor Yellow}
+}
+
+
+
