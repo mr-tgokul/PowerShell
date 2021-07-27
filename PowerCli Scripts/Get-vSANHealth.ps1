432065dc80917f0047464ea6406c39defa3028ae
Gokul T <mr.tgokul@gmail.com>
2021-07-27 12:23:57 +0530

    Function to Retrieve the vSAN Cluster Health

---
+++ PowerCli Scripts/Get-vSANHealth.ps1
@@ -0,0 +1,31 @@
+﻿Function Get-vSANHealth
+{
+
+<#
+==============================================================
+Script Created by : Gokul T
+Email : mr.tgokul@gmail.com
+===============================================================
+
+ .SYNOPSIS
+ Function to Check the vSAN Cluster health
+
+ .DESCRIPTION
+ Function to Check the vSAN Cluster health
+
+.EXAMPLE
+ Get-vSANHealth -Cluster <ClusterName>
+
+#>
+[CmdletBinding()]
+param(
+[parameter(position=0,Mandatory=$True)]
+[String]$Cluster
+)
+$ClusterHost=Get-Cluster -Name $Cluster | Get-VMHost | Select-Object -First 1
+$Esxcli=Get-EsxCli -VMHost $ClusterHost -V2
+$Esxcli.vsan.health.cluster.list.Invoke()
+}
+
+
+
